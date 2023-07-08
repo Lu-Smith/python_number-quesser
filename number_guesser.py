23dd50c17a49c4a15b19d9b5787c6033723950f7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-08 13:58:39 +0100

    Updated logic

--- number_guesser.py
+++ number_guesser.py
@@ -1,3 +1,16 @@
 import random
 
-print(random.randint(11))
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a nunber larger than 0 next time.")
+        quit()
+
+else:
+    print("Next time, please type a number.")
+    quit()
+
+random_number = random.randrange(12)
