a0a406445be98429208a076871d38339df331254
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-08 14:34:26 +0100

    Updated logic

--- number_guesser.py
+++ number_guesser.py
@@ -14,4 +14,20 @@ else:
     quit()
 
 random_number = random.randint(0, top_of_range)
-print(random_number)
+
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+        if user_guess <= 0:
+            print("Please type a number larger than 0.")
+            quit()
+    else:
+        print("Please write a number next time.")
+        quit()
+    
+    if user_guess == random_number:
+        print("You got it!")
+    else:
+        print("You got it wrong. Please, try again.")
+    break
